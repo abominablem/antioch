013ce4cbd3bc46967b513d79c612b5560a05831b
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2021-11-06 00:02:17 +0000

    Initial commit

---
+++ root.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 22:48:18 2021
+
+@author: marcu
+"""
+import tkinter as tk
+from tkinter import ttk
+
+class Antioch:
+    def __init__(self):
+        return
