28c6c4616044a50f3e92e4906012452a65b62190
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2021-11-16 00:01:15 +0000

    Added import_ofx view

--- django/antioch/import/urls.py
+++ django/antioch/import/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.import_view, name='import'),
+    path('import_ofx/', views.import_ofx, name='import-ofx'),
     ]
--- django/antioch/import/views.py
+++ django/antioch/import/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
+from .ofx_parser import OFX
 
 # Create your views here.
 
 def import_view(request):
     return render(request, 'import_view.html', context = {})
+
+def import_ofx(request):
+    filename = None #TODO
+    ofx_objects = {}
+    # create an OFX object for each file imported
+    for key, file in request.FILES.items():
+        file_text = file.read()
+        ofx_objects[file.name] = OFX().import_raw(file_text)
+        file.close()
+    context = {
+        'ofx_objects': ofx_objects
+        }
+    # print(dir(ofx_objects[list(ofx_objects.keys())[0]]))
+    # print(ofx_objects[list(ofx_objects.keys())[0]].transactions)
+    return render(request, 'preview_files.html', context = context)
